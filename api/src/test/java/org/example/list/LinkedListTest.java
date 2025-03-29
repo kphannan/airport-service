946ad84173886852170194f69dece154e0a2ac6f
Kevin Hannan <kphannan@gmail.com>
2025-03-29 10:48:48 -0400

    style: cleanup some static analysis findings

--- api/src/test/java/org/example/list/LinkedListTest.java
+++ api/src/test/java/org/example/list/LinkedListTest.java
@@ -3,7 +3,9 @@
 package org.example.list;
 
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
