ad1c368b953b6220ab71d68b02e054816d642569
Kevin Hannan <kphannan@gmail.com>
2025-05-02 18:21:47 -0400

    test: add tests for RegionDTO

--- api/src/main/java/com/example/airline/location/RegionDTO.java
+++ api/src/main/java/com/example/airline/location/RegionDTO.java
@@ -5,10 +5,18 @@ package com.example.airline.location;
 
 import java.net.URI;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.jspecify.annotations.NonNull;
+import lombok.NonNull;
 import org.jspecify.annotations.Nullable;
+//import org.jspecify.annotations.NonNull;
+//import org.jspecify.annotations.Nullable;
 
 // TODO change code to a 2 character code...
 // record CountryDTO( Long id, String code, String name, String wikiLink,
@@ -30,13 +38,35 @@ public class RegionDTO
      * persistent, even if the region code changes.
      */
     @SuppressWarnings( "PMD.ShortVariable" )
-    @NonNull private Integer id;
+    @JsonProperty( "id" )
+    @Schema( name = "id",
+             description = "Unique identifier",
+             requiredMode = Schema.RequiredMode.REQUIRED )
+    @NotNull( message = "A Region id is required" )
+    private Integer id;
 
     /**
      * local_code prefixed with the country code to make a globally unique
      * identifier.
      */
-    @NonNull private String code;
+    @JsonProperty( "code" )
+    @Schema( name = "code",
+             description =
+                     """
+                        The two-character ISO 3166:1-alpha2 code for the country,
+                        followed by a local code.
+                     """,
+             requiredMode = Schema.RequiredMode.REQUIRED,
+             minLength = 3,
+             maxLength = 7,
+             pattern = "[A-Z]{2}-[A-Z\\-]{1,4}",
+             example = "IE-D" )
+    @NotBlank( message = "A unique region code is required" )
+    @NonNull
+    // TODO also need to support 'U-A' for unassigned
+    @Pattern( regexp = "[A-Z]{2}-[A-Z\\-]{1,4}",
+              message = "Code must a valid ISO 3166:1-alpha2 followed by '-' and a local code" )
+    private String code;
 
     /**
      * The local code for the administrative subdivision. Whenever possible, these
@@ -45,36 +75,103 @@ public class RegionDTO
      * for each country, which means that the airport has not yet been assigned to a
      * region (or perhaps can't be, as in the case of a deep-sea oil platform).
      */
-    @NonNull private String localCode;
+    @JsonProperty( "localCode" )
+    @Schema( name = "localCode",
+             description =
+                     """
+                        local code.
+                     """,
+             requiredMode = Schema.RequiredMode.REQUIRED,
+             minLength = 3,
+             maxLength = 7,
+             pattern = "[A-Z]{2}-[A-Z\\-]{1,4}",
+             example = "IE-D" )
+    @NotBlank( message = "A unique local code is required" )
+    @NonNull
+    // TODO also need to support 'U-A' for unassigned
+    @Pattern( regexp = "[A-Z0-9]{1,2}[A-Z\\-]{0,5}",
+              message = "local code" )
+    private String localCode;
 
     /**
      * The common English-language name for the administrative subdivision. In some
      * cases, the name in local languages will appear in the 'keywords' field assist
      * search.
      */
-    @NonNull private String name;
+    @JsonProperty( "name" )
+    @Schema( name = "name",
+             description =
+                     """
+                        The common English-language name for the region.
+                        Other variations of the name may appear in the keywords field to assist with search.
+                     """,
+             example = "North America",
+             requiredMode = Schema.RequiredMode.REQUIRED,
+             minLength = 7,
+             maxLength = 80 )
+    @NotBlank( message = "Region name is required" )
+    @Size( min = 2, max = 52, message = "Region name must be between 7 and 80 characters" )
+    @Pattern( regexp = "[a-zA-Z][a-zA-Z ]{1,51}", message = "Region name must be between 7 and 80 characters" )
+    @NonNull
+    private String name;
 
     /**
      * The two-character ISO 3166:1-alpha2 code for the country containing the
      * administrative subdivision. A handful of unofficial, non-ISO codes are also
      * in use, such as "XK" for Kosovo.
      */
-    @NonNull private String country; // ! Create domain-object for the country code
+    @JsonProperty( "country" )
+    @Schema( name = "country",
+             description = "The two-character ISO 3166:1-alpha2 code for the country.",
+             requiredMode = Schema.RequiredMode.REQUIRED,
+             minLength = 2,
+             maxLength = 2,
+             pattern = "[A-Z]{2}",
+             example = "IE" )
+    @NotBlank( message = "An ISO 3166:1-alpha2 country code is required" )
+    @NonNull
+    @Pattern( regexp = "[A-Z]{2}", message = "Code must a valid ISO 3166:1-alpha2" )
+    private String country; // ! Create domain-object for the country code
 
     /**
      * A code for the continent to which the region belongs. See the continent field
      * in airports.csv for a list of codes.
      */
-    @NonNull private String continent; // ! Create domain-object for continent code
+    @JsonProperty( "continent" )
+    @Schema( name = "continent",
+             description = "Unique abbreviation, of the continent where this country is located.",
+             requiredMode = Schema.RequiredMode.REQUIRED,
+             minLength = 2,
+             maxLength = 2,
+             pattern = "[A-Z]{2}",
+             example = "EU" )
+    @NotBlank( message = "A 2-character continent code is required" )
+    @NonNull
+    @Pattern( regexp = "[A-Z]{2}", message = "Continent code must be 2 uppercase characters" )
+    private String continent; // ! Create domain-object for continent code
 
     /**
      * A link to the Wikipedia article describing the subdivision.
      */
-    @Nullable private URI wikipediaLink;
+    @JsonProperty( "wikiLink" )
+    @Schema( name = "wikiLink",
+             description = "Link to the Wikipedia article about the country",
+             example = "https://en.wikipedia.org/wiki/County_Dublin",
+             requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+             maxLength = 255 )
+    @Nullable
+    private URI wikipediaLink;
 
     /**
      * A comma-separated list of keywords for helping with search. May include former
      * names for the region, and/or the region name in other languages.
      */
-    @Nullable private String keywords;
+    @JsonProperty( "keywords" )
+    @Schema( name = "keywords",
+             description = "Optional additional search terms",
+             requiredMode = Schema.RequiredMode.NOT_REQUIRED,
+             maxLength = 255 )
+    @Nullable
+    @Size( max = 255, message = "List of keywords may not exceed 255 characters" )
+    private String keywords;
 }
---
+++ api/src/test/java/com/example/airline/location/RegionDTOTest.java
@@ -0,0 +1,236 @@
+/* (C) 2025 */
+
+package com.example.airline.location;
+
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.tuple;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.net.URI;
+import java.util.Set;
+
+import com.example.rest.validation.ConstraintValidationUtility;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Unit tests for the {@code RegionDTO}.
+ */
+@SuppressWarnings( "PMD.AvoidDuplicateLiterals" )
+class RegionDTOTest
+{
+    private Validator validator;
+    private URI       testURI;
+
+    @BeforeEach
+    void setUp()
+    {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+
+        validator = factory.getValidator();
+
+        testURI = URI.create( "http://test.domain/with/a/path" );
+    }
+
+    @Nested
+    @DisplayName( "constructor will" )
+    class Constructor
+    {
+        @Test
+        @DisplayName( "throw an exception when the region primary id is null" )
+        void region_nullArgs_throwsNullPointer()
+        {
+            Throwable thrown = assertThrows( IllegalArgumentException.class,
+                                             () -> new RegionDTO( null, null, null, null, null, null, null, null )
+                                           );
+            assertThat( thrown.getMessage() )
+                    .contains( "code is marked non-null but is null" );
+        }
+
+        @Test
+        @DisplayName( "reject null for the continent code" )
+        void constructor_NullArgs_throwsNullPointer()   // TODO change lombok config to throw IllegalArgument instead of default NPE
+        {
+            final Throwable thrown = assertThrows( IllegalArgumentException.class,
+                                                   () -> new RegionDTO( 1, null, null, null, null, null, null, null )
+                                                 );
+            assertEquals( "code is marked non-null but is null", thrown.getMessage() );
+        }
+
+        @Test
+        @DisplayName( "accept minimum required arguments" )
+        void constructor_withArgs_DoesNotCrash()
+        {
+            final RegionDTO dto = new RegionDTO( null, "IE-D", "D", "County Dublin", "IE", "EU", null, null );
+
+            assertNotNull( dto );
+        }
+
+        @Test
+        @DisplayName( "reject null for the continent code" )
+        void constructor_CodeNull_throwsNullPointer()   // TODO change lombok config to throw IllegalArgument instead of default NPE
+        {
+            final Throwable thrown = assertThrows( IllegalArgumentException.class,
+                                                   () -> new RegionDTO( null, null, "D", "County Dublin", "IE", "EU", null, null )
+                                                 );
+            assertEquals( "code is marked non-null but is null", thrown.getMessage() );
+        }
+
+        @Test
+        @DisplayName( "reject null for the continent name" )
+        void constructor_NameNull_throws()
+        {
+            final Throwable thrown = assertThrows( IllegalArgumentException.class,
+                                                   () -> new RegionDTO( null, "IE-D", "D", null, "IE", "EU", null, null )
+                                                 );
+            assertEquals( "name is marked non-null but is null", thrown.getMessage() );
+        }
+    }
+
+
+    @Nested
+    @DisplayName( "NewRegionDTO - Validation" )
+    class ValidationGroup
+    {
+        @Test
+        @DisplayName( "reject a blank continent code" )
+        void region_blankId_returnsViolation()
+        {
+            RegionDTO itemUnderTest = new RegionDTO( null,
+                                                     "CC",
+                                                     "LOCAL",
+                                                     "Name",
+                                                     "IE",
+                                                     "EU",
+                                                     testURI,
+                                                     "Key1, key2" );
+            Set<ConstraintViolation<RegionDTO>> constraintViolations = validator.validate( itemUnderTest );
+
+            ConstraintValidationUtility
+                    .assertConstraintErrors( constraintViolations,
+                                             tuple( "code", "Code must a valid ISO 3166:1-alpha2 followed by '-' and a local code" ),
+                                             tuple( "id", "A Region id is required" )
+                                           );
+        }
+
+        @Test
+        @DisplayName( "reject a blank continent code" )
+        void region_blankCode_returnsViolation()
+        {
+            RegionDTO itemUnderTest = new RegionDTO( 1,
+                                                     "  ",
+                                                     "LOCAL",
+                                                     "Name",
+                                                     "IE",
+                                                     "EU",
+                                                     testURI,
+                                                     "Key1, key2" );
+            Set<ConstraintViolation<RegionDTO>> constraintViolations = validator.validate( itemUnderTest );
+
+            ConstraintValidationUtility
+                    .assertConstraintErrors( constraintViolations,
+                                             tuple( "code", "A unique region code is required" ),
+                                             tuple( "code", "Code must a valid ISO 3166:1-alpha2 followed by '-' and a local code" )
+                                           );
+        }
+
+        @Test
+        @DisplayName( "reject single character code" )
+        void region_singleCharCode_returnsViolation()
+        {
+            RegionDTO itemUnderTest = new RegionDTO( 1,
+                                                     "A",
+                                                     "LOCAL",
+                                                     "Name",
+                                                     "IE",
+                                                     "EU",
+                                                     null,
+                                                     null );
+            Set<ConstraintViolation<RegionDTO>> constraintViolations = validator.validate( itemUnderTest );
+
+            ConstraintValidationUtility
+                    .assertConstraintErrors( constraintViolations,
+                                             tuple( "code", "Code must a valid ISO 3166:1-alpha2 followed by '-' and a local code" )
+                                           );
+        }
+
+        @Test
+        @DisplayName( "reject numeric code" )
+        void region_digitCode_returnsViolation()
+        {
+            RegionDTO itemUnderTest = new RegionDTO( 1,
+                                                     "42",
+                                                     "LOCAL",
+                                                     "Name",
+                                                     "IE",
+                                                     "EU",
+                                                     null,
+                                                     null );
+            Set<ConstraintViolation<RegionDTO>> constraintViolations = validator.validate( itemUnderTest );
+
+            ConstraintValidationUtility
+                    .assertConstraintErrors( constraintViolations,
+                                             tuple( "code", "Code must a valid ISO 3166:1-alpha2 followed by '-' and a local code" )
+                                           );
+        }
+
+        @Test
+        @DisplayName( "reject a blank continent name" )
+        void region_nullName_returnsViolation()
+        {
+            RegionDTO itemUnderTest = new RegionDTO( 1,
+                                                     "AA",
+                                                     "LOCAL",
+                                                     "   ",
+                                                     "IE",
+                                                     "EU",
+                                                     null,
+                                                     null );
+
+            Set<ConstraintViolation<RegionDTO>> constraintViolations = validator.validate( itemUnderTest );
+
+            ConstraintValidationUtility
+                    .assertConstraintErrors( constraintViolations,
+                                             tuple( "code", "Code must a valid ISO 3166:1-alpha2 followed by '-' and a local code" ),
+                                             tuple( "name", "Region name is required" ),
+                                             tuple( "name", "Region name must be between 7 and 80 characters" ) );
+        }
+
+        @Test
+        @DisplayName( "reject a blank continent code" )
+        void region_missingAllRequired_returnsViolation()
+        {
+            RegionDTO itemUnderTest = new RegionDTO( null,
+                                                     "  ",
+                                                     "  ",
+                                                     "   ",
+                                                     "IE",
+                                                     "EU",
+                                                     testURI,
+                                                     "Key1, key2" );
+            Set<ConstraintViolation<RegionDTO>> constraintViolations = validator.validate( itemUnderTest );
+
+            ConstraintValidationUtility
+                    .assertConstraintErrors( constraintViolations,
+                                             tuple( "id", "A Region id is required" ),
+                                             tuple( "code", "Code must a valid ISO 3166:1-alpha2 followed by '-' and a local code" ),
+                                             tuple( "code", "A unique region code is required" ),
+                                             tuple( "localCode", "A unique local code is required" ),
+                                             tuple( "localCode", "local code" ),
+                                             tuple( "name", "Region name is required" ),
+                                             tuple( "name", "Region name must be between 7 and 80 characters" )
+                                           );
+        }
+    }
+
+}
