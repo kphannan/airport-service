e40b0925089e9ee864b25a180daaefbdfbd4d337
Kevin Hannan <kphannan@gmail.com>
2026-03-08 18:00:05 -0400

    test: Add simple test for the EntityMapHelper utility class

---
+++ app/src/test/java/com/example/airline/location/continent/mapper/EntityMapHelperTest.java
@@ -0,0 +1,25 @@
+package com.example.airline.location.continent.mapper;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.StringJoiner;
+
+import com.example.utility.ValidateUtilityClass;
+import org.junit.jupiter.api.Test;
+
+class EntityMapHelperTest
+{
+    // TODO include UtilityClass Verification
+
+    @Test
+    void isProperUtilityClass()
+    {
+        final StringJoiner reason = new StringJoiner( ", ", "[", "]" );
+
+        assertAll( () -> assertTrue( ValidateUtilityClass.isProperUtilityClass( EntityMapHelper.class, reason ) ),
+                   () -> assertEquals( "[]", reason.toString() ) );
+
+    }
+}
