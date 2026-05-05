d6936792cee1f4835f10f2eb24a62a5a3abc11e4
Kevin Hannan <kphannan@gmail.com>
2026-05-05 14:51:31 -0400

    test: Increase code coverage for log printing.

--- lib/src/test/java/com/example/utility/HeaderUtilityTest.java
+++ lib/src/test/java/com/example/utility/HeaderUtilityTest.java
@@ -1,12 +1,13 @@
 package com.example.utility;
 
 import static com.example.utility.HeaderUtility.copyNeededHeaders;
+import static com.example.utility.HeaderUtility.createHeaders;
+import static com.example.utility.HeaderUtility.printHeaders;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.net.URI;
 import java.security.Principal;
 import java.util.Arrays;
 import java.util.Collections;
@@ -18,17 +19,15 @@ import java.util.Map;
 import java.util.StringJoiner;
 import java.util.stream.Stream;
 
-import jakarta.servlet.http.HttpServletRequest;
+import nl.altindag.log.LogCaptor;
 import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.support.HttpRequestWrapper;
 import org.springframework.test.http.HttpHeadersAssert;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 
 /**
@@ -40,23 +39,32 @@ import org.springframework.web.context.request.WebRequest;
  */
 class HeaderUtilityTest
 {
+    HttpHeaders headersUnderTest = new HttpHeaders();
+
+    @BeforeEach
+    void init()
+    {
+        headersUnderTest = new HttpHeaders();
+    }
 
     @Test
     void copyHeaders_nullFilter_returnsOriginalHeaders()
     {
-        HttpHeaders headers = new HttpHeaders();
-        HttpHeaders result = copyNeededHeaders( headers, null );
-        assertThat( result )
-                .isEqualTo( headers );
+        // --- given
+        // --- when
+        HttpHeaders result = copyNeededHeaders( headersUnderTest, null );
+
+        // --- then
+        final HttpHeadersAssert headersAssert = new HttpHeadersAssert( result );
+        assertThat( headersAssert.isEqualTo( headersUnderTest ) );
     }
 
     @Test
     void copyHeaders_emptyFilter_returnsOriginalHeaders()
     {
-        HttpHeaders headers = new HttpHeaders();
-        HttpHeaders result = copyNeededHeaders( headers, Collections.emptyList() );
-        assertThat( result )
-                .isEqualTo( headers );
+        HttpHeaders result = copyNeededHeaders( headersUnderTest, Collections.emptyList() );
+        final HttpHeadersAssert headersAssert = new HttpHeadersAssert( result );
+        assertThat( headersAssert.isSameAs( headersUnderTest ) );
     }
 
     @Test
@@ -69,17 +77,16 @@ class HeaderUtilityTest
                                                     "Allow",
                                                     "Accept-Encoding" );
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set( "Accept", MediaType.APPLICATION_JSON_VALUE );
-        headers.set( "Content-Type", "application/json;charset=UTF-8" );
-        headers.set( HttpHeaders.ACCEPT_LANGUAGE, "en-US" );
-        headers.set( HttpHeaders.ACCEPT_CHARSET, "utf-8" );
-        headers.set( HttpHeaders.ACCEPT_ENCODING, "gzip" );
-        headers.set( "TRACEPARENT", "traceParent" );
-        headers.set( "TRACESTATE", "traceState" );
+        headersUnderTest.set( "Accept", MediaType.APPLICATION_JSON_VALUE );
+        headersUnderTest.set( "Content-Type", "application/json;charset=UTF-8" );
+        headersUnderTest.set( HttpHeaders.ACCEPT_LANGUAGE, "en-US" );
+        headersUnderTest.set( HttpHeaders.ACCEPT_CHARSET, "utf-8" );
+        headersUnderTest.set( HttpHeaders.ACCEPT_ENCODING, "gzip" );
+        headersUnderTest.set( "TRACEPARENT", "traceParent" );
+        headersUnderTest.set( "TRACESTATE", "traceState" );
 
         // --- when
-        HttpHeaders result = copyNeededHeaders( headers, copiedHeaders );
+        HttpHeaders result = copyNeededHeaders( headersUnderTest, copiedHeaders );
 
         // --- then
         final HttpHeadersAssert headersAssert = new HttpHeadersAssert( result );
@@ -96,17 +103,16 @@ class HeaderUtilityTest
     void copyUsualHeaders_returnsFilteredHeaders()
     {
         // -- given
-        HttpHeaders headers = new HttpHeaders();
-        headers.set( "Accept", MediaType.APPLICATION_JSON_VALUE );
-        headers.set( "Content-Type", "application/json;charset=UTF-8" );
-        headers.set( HttpHeaders.ACCEPT_LANGUAGE, "en-US" );
-        headers.set( HttpHeaders.ACCEPT_CHARSET, "utf-8" );
-        headers.set( HttpHeaders.ACCEPT_ENCODING, "gzip" );
-        headers.set( "TRACEPARENT", "traceParent" );
-        headers.set( "TRACESTATE", "traceState" );
+        headersUnderTest.set( "Accept", MediaType.APPLICATION_JSON_VALUE );
+        headersUnderTest.set( "Content-Type", "application/json;charset=UTF-8" );
+        headersUnderTest.set( HttpHeaders.ACCEPT_LANGUAGE, "en-US" );
+        headersUnderTest.set( HttpHeaders.ACCEPT_CHARSET, "utf-8" );
+        headersUnderTest.set( HttpHeaders.ACCEPT_ENCODING, "gzip" );
+        headersUnderTest.set( "TRACEPARENT", "traceParent" );
+        headersUnderTest.set( "TRACESTATE", "traceState" );
 
         // --- when
-        HttpHeaders result = copyNeededHeaders( headers );
+        HttpHeaders result = copyNeededHeaders( headersUnderTest );
 
         // --- then
         final HttpHeadersAssert headersAssert = new HttpHeadersAssert( result );
@@ -135,184 +141,6 @@ class HeaderUtilityTest
     }
 
 
-    @Test
-    void copyHeaders_fromWebRequest_returnsFilteredHeaders()
-    {
-        WebRequest  webRequest = new WebRequest()
-        {
-            @Override
-            public @Nullable Object getAttribute( String name, int scope )
-            {
-                return null;
-            }
-
-            @Override
-            public void setAttribute( String name, Object value, int scope )
-            {
-
-            }
-
-            @Override
-            public void removeAttribute( String name, int scope )
-            {
-
-            }
-
-            @Override
-            public String[] getAttributeNames( int scope )
-            {
-                return new String[0];
-            }
-
-            @Override
-            public void registerDestructionCallback( String name, Runnable callback, int scope )
-            {
-
-            }
-
-            @Override
-            public @Nullable Object resolveReference( String key )
-            {
-                return null;
-            }
-
-            @Override
-            public String getSessionId()
-            {
-                return "";
-            }
-
-            @Override
-            public Object getSessionMutex()
-            {
-                return null;
-            }
-
-            @Override
-            public @Nullable String getHeader( String headerName )
-            {
-                Map<String, String[]> foo = new HashMap<>();
-                foo.put( "TRACEPARENT", Stream.of( "traceParent" ).toArray( String[]::new ) );
-                foo.put( "TRACESTATE", Stream.of( "traceState" ).toArray( String[]::new ) );
-                foo.put( "Content-Type", Stream.of("application/json;charset=UTF-8").toArray( String[]::new ) );
-
-                return foo.containsKey( headerName ) ? foo.get( headerName )[0] : "";
-            }
-
-            @Override
-            public String @Nullable [] getHeaderValues( String headerName )
-            {
-//                Map<String, String[]> foo = new HashMap<>();
-//                foo.put( "TRACEPARENT", Stream.of( "traceParent" ).toArray( String[]::new ) );
-//                foo.put( "TRACESTATE", Stream.of( "traceState" ).toArray( String[]::new ) );
-//
-//                return foo.get( headerName );
-                return new String[0];
-            }
-
-            @Override
-            public Iterator<String> getHeaderNames()
-            {
-                return null;
-            }
-
-            @Override
-            public @Nullable String getParameter( String paramName )
-            {
-                return "";
-            }
-
-            @Override
-            public String @Nullable [] getParameterValues( String paramName )
-            {
-                return new String[0];
-            }
-
-            @Override
-            public Iterator<String> getParameterNames()
-            {
-                return null;
-            }
-
-            @Override
-            public Map<String, String[]> getParameterMap()
-            {
-                return Map.of();
-            }
-
-            @Override
-            public Locale getLocale()
-            {
-                return null;
-            }
-
-            @Override
-            public String getContextPath()
-            {
-                return "";
-            }
-
-            @Override
-            public @Nullable String getRemoteUser()
-            {
-                return "";
-            }
-
-            @Override
-            public @Nullable Principal getUserPrincipal()
-            {
-                return null;
-            }
-
-            @Override
-            public boolean isUserInRole( String role )
-            {
-                return false;
-            }
-
-            @Override
-            public boolean isSecure()
-            {
-                return false;
-            }
-
-            @Override
-            public boolean checkNotModified( long lastModifiedTimestamp )
-            {
-                return false;
-            }
-
-            @Override
-            public boolean checkNotModified( String etag )
-            {
-                return false;
-            }
-
-            @Override
-            public boolean checkNotModified( @Nullable String etag, long lastModifiedTimestamp )
-            {
-                return false;
-            }
-
-            @Override
-            public String getDescription( boolean includeClientInfo )
-            {
-                return "";
-            }
-        };
-
-        HttpHeaders       headers = copyNeededHeaders( webRequest );
-
-
-        final HttpHeadersAssert headersAssert = new HttpHeadersAssert( headers );
-        assertAll( () -> headersAssert
-                           .hasValue( "TRACEPARENT", "traceParent" )
-                           .hasValue( "TRACESTATE", "traceState" )
-                           .hasValue( "Content-Type", "application/json;charset=UTF-8" )
-                           .doesNotContainHeader( "NoWay" )
-                 );
-    }
-
 
     @Test
     @DisplayName( "Is a Proper Utility class" )
@@ -324,5 +152,244 @@ class HeaderUtilityTest
                    () -> assertEquals( "[]", reason.toString() ) );
 
     }
+
+
+    @Nested
+    @DisplayName( "Log output" )
+    class LogOutput
+    {
+        @SuppressWarnings( "initialization.field.uninitialized" )
+        private static LogCaptor logCaptor; // will be set in @BeforeAll
+        @SuppressWarnings( "initialization.field.uninitialized" )
+        private StringJoiner     reason;    // will be set in @BeforeEach
+
+        @BeforeEach
+        void init()
+        {
+            logCaptor = LogCaptor.forClass( HeaderUtility.class );
+        }
+
+        @Test
+        void logOutput_printHeaders()
+        {
+            // --- given
+            headersUnderTest.set( "TestPrint", "kilroy" );
+
+            // --- when
+            printHeaders( headersUnderTest );
+
+            // --- then
+            var logContent = logCaptor.getLogs();
+
+            assertThat( logContent ).contains( "TestPrint : [kilroy]" );
+        }
+
+    }
+
+    @Nested
+    @DisplayName( "From WebRequest" )
+    class FromWebRequest
+    {
+        private WebRequest webRequest;
+
+        @BeforeEach
+        void init()
+        {
+            webRequest = new WebRequest()
+            {
+                @Override
+                public @Nullable Object getAttribute( String name, int scope )
+                {
+                    return null;
+                }
+
+                @Override
+                public void setAttribute( String name, Object value, int scope )
+                {
+
+                }
+
+                @Override
+                public void removeAttribute( String name, int scope )
+                {
+
+                }
+
+                @Override
+                public String[] getAttributeNames( int scope )
+                {
+                    return new String[0];
+                }
+
+                @Override
+                public void registerDestructionCallback( String name, Runnable callback, int scope )
+                {
+
+                }
+
+                @Override
+                public @Nullable Object resolveReference( String key )
+                {
+                    return null;
+                }
+
+                @Override
+                public String getSessionId()
+                {
+                    return "";
+                }
+
+                @Override
+                public Object getSessionMutex()
+                {
+                    return null;
+                }
+
+                @Override
+                public @Nullable String getHeader( String headerName )
+                {
+                    Map<String, String[]> foo = new HashMap<>();
+                    foo.put( "TRACEPARENT", Stream.of( "traceParent" ).toArray( String[]::new ) );
+                    foo.put( "TRACESTATE", Stream.of( "traceState" ).toArray( String[]::new ) );
+                    foo.put( "Content-Type", Stream.of( "application/json;charset=UTF-8" ).toArray( String[]::new ) );
+
+                    return foo.containsKey( headerName ) ? foo.get( headerName )[0] : "";
+                }
+
+                @Override
+                public String @Nullable [] getHeaderValues( String headerName )
+                {
+//                Map<String, String[]> foo = new HashMap<>();
+//                foo.put( "TRACEPARENT", Stream.of( "traceParent" ).toArray( String[]::new ) );
+//                foo.put( "TRACESTATE", Stream.of( "traceState" ).toArray( String[]::new ) );
+//
+//                return foo.get( headerName );
+                    return new String[0];
+                }
+
+                @Override
+                public Iterator<String> getHeaderNames()
+                {
+                    return null;
+                }
+
+                @Override
+                public @Nullable String getParameter( String paramName )
+                {
+                    return "";
+                }
+
+                @Override
+                public String @Nullable [] getParameterValues( String paramName )
+                {
+                    return new String[0];
+                }
+
+                @Override
+                public Iterator<String> getParameterNames()
+                {
+                    return null;
+                }
+
+                @Override
+                public Map<String, String[]> getParameterMap()
+                {
+                    return Map.of();
+                }
+
+                @Override
+                public Locale getLocale()
+                {
+                    return null;
+                }
+
+                @Override
+                public String getContextPath()
+                {
+                    return "";
+                }
+
+                @Override
+                public @Nullable String getRemoteUser()
+                {
+                    return "";
+                }
+
+                @Override
+                public @Nullable Principal getUserPrincipal()
+                {
+                    return null;
+                }
+
+                @Override
+                public boolean isUserInRole( String role )
+                {
+                    return false;
+                }
+
+                @Override
+                public boolean isSecure()
+                {
+                    return false;
+                }
+
+                @Override
+                public boolean checkNotModified( long lastModifiedTimestamp )
+                {
+                    return false;
+                }
+
+                @Override
+                public boolean checkNotModified( String etag )
+                {
+                    return false;
+                }
+
+                @Override
+                public boolean checkNotModified( @Nullable String etag, long lastModifiedTimestamp )
+                {
+                    return false;
+                }
+
+                @Override
+                public String getDescription( boolean includeClientInfo )
+                {
+                    return "";
+                }
+            };
+        }
+
+        @Test
+        @DisplayName( "copy and filter headers" )
+        void copyHeaders_fromWebRequest_returnsFilteredHeaders()
+        {
+            HttpHeaders headers = copyNeededHeaders( webRequest );
+
+            final HttpHeadersAssert headersAssert = new HttpHeadersAssert( headers );
+            assertAll( () -> headersAssert
+                               .hasValue( "TRACEPARENT", "traceParent" )
+                               .hasValue( "TRACESTATE", "traceState" )
+                               .hasValue( "Content-Type", "application/json;charset=UTF-8" )
+                               .doesNotContainHeader( "NoWay" )
+                     );
+        }
+
+
+        @Test
+        @DisplayName( "create default headers" )
+        void copyHeaders_fromNullWebRequest_returnsEmptyHeaders()
+        {
+            // --- given
+
+            // --- when
+            final HttpHeaders headers = createHeaders( null );
+
+            // --- then
+            final HttpHeadersAssert headersAssert = new HttpHeadersAssert( headers );
+            assertAll( headersAssert::isEmpty );
+        }
+
+    }
+
 
 }
